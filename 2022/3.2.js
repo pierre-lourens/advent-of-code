ee745688c17d81cc318f613590f8f9755a7fe8c2
Pierre Lourens <plourens3@gmail.com>
2022-12-02 22:16:52 -0800

    minor cleanup

--- 2022/3.2.js
+++ 2022/3.2.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const data = fs.readFileSync(`${__dirname}/input-3.txt`, 'utf8');
+const data = fs.readFileSync(`${__dirname}/input/input-3.txt`, 'utf8');
 const lines = data.split(/\n/);
 
 let sum = 0;
@@ -8,23 +8,23 @@ for (let i = 0; i < lines.length; i += 3) {
   for (let j = 0; j < lines[i].length; j++) {
     record.push(lines[i][j]);
   }
+  const second = lines[i + 1].split('');
+  const third = lines[i + 2].split('');
   record = record.filter((letter) => {
     // letter has to be in 2nd and 3rd line
-    const second = lines[i + 1].split('');
-    const third = lines[i + 2].split('');
     const inSecond = second.some((let) => let === letter);
     const inThird = third.some((let) => let === letter);
     return inSecond && inThird;
   });
   const letter = record[0];
   const code = letter.charCodeAt(0);
+  let priority;
   if (code >= 97) {
-    const priority = code - 96;
-    sum += priority;
+    priority = code - 96;
   } else {
-    const priority = code - 38;
-    sum += priority;
+    priority = code - 38;
   }
+  sum += priority;
 }
 
 console.log({ sum });
